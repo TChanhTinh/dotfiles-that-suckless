841987f677749af5c2bc7248fb520c9128b0278b
owl4ce <findarr@pm.me>
2021-02-15 21:40:04 +0700

    cringe: rename wallpaper

--- homedir/.dwm/autostart.sh
+++ homedir/.dwm/autostart.sh
@@ -5,7 +5,7 @@ source $HOME/.owl4ce_var
     xrdb $HOME/.Xresources
     dwmblocks  &
     pulseaudio &
-    nitrogen --set-zoom-fill --save $HOME/.wallpaper/mechanical/EPIC_2K.jpg &
+    nitrogen --set-zoom-fill --save $HOME/.wallpaper/mechanical/autumn_3K.jpg &
     picom -b &
     dunst -conf $HOME/.config/dunst/dunstrc-mech-MINMOD &
     lxpolkit &> /dev/null &
