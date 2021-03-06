ea710a8e9c5b98bb860c2a6d08c78bf2a38b3d20
tchanhtinh@gmail.com <tchanhtinh@gmail.com>
2021-03-06 15:03:08 +0700

    update new screenshot

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -157,6 +157,4 @@ static Button buttons[] = {
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
